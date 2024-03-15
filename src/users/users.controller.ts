69543080663a3fcce1221a66d88983f4173f96cb
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-14 22:00:06 -0400

    [FEAT]added update and delete user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -23,9 +23,9 @@ export class UsersController {
 
   @UseGuards(AuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string) {
     try {
-      return this.usersService.findOne(id);
+      return await this.usersService.findOne(id);
     } catch (error) {
       throw new BadRequestException(error.message, {
         cause: new Error(error.message),
@@ -34,12 +34,24 @@ export class UsersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    try {
+      return await this.usersService.update(id, updateUserDto);
+    } catch (error) {
+      throw new BadRequestException(error.message, {
+        cause: new Error(error.message),
+      });
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async remove(@Param('id') id: string) {
+    try {
+      return await this.usersService.remove(id);
+    } catch (error) {
+      throw new BadRequestException(error.message, {
+        cause: new Error(error.message),
+      });
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -33,11 +33,11 @@ export class UsersService {
     return await this.userModel.findById(id).exec();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user ${updateUserDto}`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return await this.userModel.updateOne({ _id: id }, updateUserDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    return await this.userModel.deleteOne({ _id: id });
   }
 }
