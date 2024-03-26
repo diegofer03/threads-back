e5512ea4c2e0c446ee39016bf24bce3a3669a18a
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-25 21:54:12 -0400

    [FEAT]expiration date jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: async (configService: ConfigService) => ({
         global: true,
         secret: configService.get<string>('SECRET_KEY'),
-        signOptions: { expiresIn: '300s' },
+        signOptions: { expiresIn: '24h' },
       }),
       inject: [ConfigService],
     }),
